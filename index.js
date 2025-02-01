3702da96780f478e0eeca0650b02a3431297f66d
Rama <rassyabr79@gmail.com>
2025-02-01 09:29:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import cp from 'child_process'
 import os from 'os'
 
 const app = express()
-const PORT = process.env.PORT || 3000 // Gunakan PORT dari environment atau default 3000
 
-// Fungsi untuk memformat ukuran file/memori
 function formatSize(bytes) {
     if (bytes < 1024) return bytes + ' B'
     const k = 1024
@@ -15,27 +13,17 @@ function formatSize(bytes) {
     return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i - 1]
 }
 
-// Fungsi untuk memformat tanggal
-function formatDate(n, locale = 'id') {
-    const d = new Date(n)
-    return d.toLocaleDateString(locale, { timeZone: 'Asia/Jakarta' })
-}
-
-// Endpoint utama
 app.get('/', (req, res) => res.send('Hello World!'))
 
-// Endpoint untuk status server
-app.get('/status', async (req, res) => {
+app.get('/status', (req, res) => {
     const status = {}
 
-    // Penggunaan disk
     try {
         status['diskUsage'] = cp.execSync('du -sh').toString().split('M')[0] + ' MB'
     } catch (error) {
         status['diskUsage'] = 'Tidak tersedia'
     }
 
-    // Penggunaan memori
     const used = process.memoryUsage()
     for (let x in used) {
         status[x] = formatSize(used[x])
@@ -53,17 +41,5 @@ app.get('/status', async (req, res) => {
     })
 })
 
-// Fungsi untuk menjaga server tetap hidup
-function keepAlive() {
-    const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
-    if (/(\/\/|\.)undefined\./.test(url)) return
-    setInterval(() => {
-        fetch(url).catch(console.log)
-    }, 30 * 1000)
-}
-
-// Jalankan server
-app.listen(PORT, () => {
-    keepAlive()
-    console.log(`Server berjalan di port ${PORT}`)
-})
+// Ekspor sebagai handler untuk Vercel
+export default (req, res) => app(req, res)
